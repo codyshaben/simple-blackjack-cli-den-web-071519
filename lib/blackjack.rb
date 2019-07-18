255f272e9705038d884e2bb95d3516be2b6570e1
Cody Shaben <codyshaben@Codys-MacBook-Air.local>
2019-07-18 10:45:25 -0600

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,74 @@
+require "pry"
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+ card = rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(number)
+  puts "Your cards add up to #{number}"
 end
+
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp 
 end
 
-def end_game
-  # code #end_game here
+def end_game(number)
+  puts "Sorry, you hit #{number}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  initial_cards = (deal_card + deal_card)
+  display_card_total(initial_cards)
+  initial_cards
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
+
+def hit?(total)
+  prompt_user
+  input =  get_user_input
+  if input == "s"
+    total
+  elsif input == "h"
+    total += deal_card 
+  else
+    invalid_command
+  end
+  total
+end
+
+
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+  final_cards = initial_round
+  welcome
+  
+  while final_cards <= 21 do
+    final_cards = hit?(final_cards) 
+    display_card_total(final_cards)
+  end
+  end_game(final_cards)
 end
-    
+
+
+
+
+# binding.pry
+
+ 
